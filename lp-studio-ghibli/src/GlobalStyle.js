a0bdeda7a13163a4042e2f8c3f4503cc5eeba38d
DiogoB0mfim <diogohbomfim@gmail.com>
2022-09-01 22:37:48 -0300

    finalizado

--- lp-studio-ghibli/src/GlobalStyle.js
+++ lp-studio-ghibli/src/GlobalStyle.js
@@ -3,13 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 *{
    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;900&display=swap');
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
    font-size: 16px;
 }
 
 body {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
    background: linear-gradient(to right, #628E75 , #1A4855);
 }
 
